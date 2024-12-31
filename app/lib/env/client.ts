c2c9e79eaecd944ebf8fec4c6900926b82f7002f
castrogusttavo <gusttavoleandro2023@gmail.com>
2024-12-31 14:57:53 -0300

    chore: remove Deno env

--- app/lib/env/client.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-import { NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY } from './env.ts';
-
-export const clientEnv = {
-  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z
-    .string()
-    .min(1)
-    .parse(NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY),
-};
--- deploy-vercel.sh
+++ deploy-vercel.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-# Install deno
+# Install Deno
 curl -fsSL https://deno.land/x/install/install.sh | sh
 
-# Install pagic
+# Install Pagic
 /vercel/.deno/bin/deno install --unstable --allow-read --allow-write --allow-net https://deno.land/x/pagic/mod.ts
 
 # Pagic build
 /vercel/.deno/bin/deno run --unstable --allow-read --allow-write --allow-net --allow-env --allow-run https://deno.land/x/pagic/mod.ts build
+
+# Deploy to Vercel (assuming Vercel CLI is installed and configured)
+vercel --prod
