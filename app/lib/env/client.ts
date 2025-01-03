f96d29124f3a60c8d81c628434a4d736918fe986
castrogusttavo <gusttavoleandro2023@gmail.com>
2025-01-03 00:46:11 -0300

    chore: add env (client and server) config

---
+++ app/lib/env/client.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+import { NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY } from './env';
+
+export const clientEnv = {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z
+        .string()
+        .min(1)
+        .parse(NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY),
+}
