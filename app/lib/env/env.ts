ef554229e065625b3742093e9ec6365697006026
castrogusttavo <gusttavoleandro2023@gmail.com>
2025-01-03 00:58:07 -0300

    fix: add optional function in zod validation

--- app/lib/env/env.ts
+++ app/lib/env/env.ts
@@ -9,8 +9,8 @@ const env = {
 
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
-  OPEN_AI_API_KEY: z.string().min(1),
-  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
+  OPEN_AI_API_KEY: z.string().min(1).optional(),
+  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1).optional(),
 });
 
 export const validatedEnv = envSchema.parse(env);
