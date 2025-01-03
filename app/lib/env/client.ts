5440ec5f0c76a0d3ed6ad25b7f8c0ea886668015
castrogusttavo <gusttavoleandro2023@gmail.com>
2025-01-03 00:47:19 -0300

    chore: add env (client and server) config

--- app/lib/env/client.ts
+++ app/lib/env/client.ts
@@ -2,8 +2,8 @@ import { z } from 'zod';
 import { NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY } from './env';
 
 export const clientEnv = {
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z
-        .string()
-        .min(1)
-        .parse(NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY),
-}
+  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z
+    .string()
+    .min(1)
+    .parse(NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY),
+};
--- app/lib/env/env.ts
+++ app/lib/env/env.ts
@@ -1,21 +1,22 @@
 import { z } from 'zod';
 
 const env = {
-    DATABASE_URL: process.env.DATABASE_URL,
-    OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  DATABASE_URL: process.env.DATABASE_URL,
+  OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
+  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+    process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
 };
 
 const envSchema = z.object({
-    DATABASE_URL: z.string().url(),
-    OPEN_AI_API_KEY: z.string().min(1),
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
+  DATABASE_URL: z.string().url(),
+  OPEN_AI_API_KEY: z.string().min(1),
+  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
 });
 
 export const validatedEnv = envSchema.parse(env);
 
 export const {
-    DATABASE_URL,
-    OPEN_AI_API_KEY,
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  DATABASE_URL,
+  OPEN_AI_API_KEY,
+  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
 } = validatedEnv;
