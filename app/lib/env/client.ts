2cdfc980e972766a5e89c1d27e9012438b362425
castrogusttavo <gusttavoleandro2023@gmail.com>
2025-01-12 12:05:15 -0300

    chore: add optional change in zod-env

--- app/lib/env/client.ts
+++ app/lib/env/client.ts
@@ -1,10 +1,8 @@
-import {z} from 'zod';
-import {NEXT_PUBLIC_GITHUB_API_URL} from './env';
+import { z } from 'zod';
+import { NEXT_PUBLIC_GITHUB_API_URL } from './env';
 
 export const clientEnv = {
-  NEXT_PUBLIC_GITHUB_API_URL: z.optional(z
-    .string()
-    .url()
-    .parse(NEXT_PUBLIC_GITHUB_API_URL),
-  )
+  NEXT_PUBLIC_GITHUB_API_URL: z.optional(
+    z.string().url().parse(NEXT_PUBLIC_GITHUB_API_URL),
+  ),
 };
