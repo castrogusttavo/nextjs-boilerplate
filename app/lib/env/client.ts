e7e73fbb6675f6bd008a5ea936c80667dd3d2222
castrogusttavo <gusttavoleandro2023@gmail.com>
2025-01-12 12:11:39 -0300

    chore: remove optional change in env read file

--- app/lib/env/client.ts
+++ app/lib/env/client.ts
@@ -1,8 +1,9 @@
-import { z } from 'zod';
-import { NEXT_PUBLIC_GITHUB_API_URL } from './env';
+import {z} from 'zod';
+import {NEXT_PUBLIC_GITHUB_API_URL} from './env';
 
 export const clientEnv = {
-  NEXT_PUBLIC_GITHUB_API_URL: z.optional(
-    z.string().url().parse(NEXT_PUBLIC_GITHUB_API_URL),
-  ),
+  NEXT_PUBLIC_GITHUB_API_URL: z
+    .string()
+    .url()
+    .parse(NEXT_PUBLIC_GITHUB_API_URL),
 };
--- app/lib/env/env.ts
+++ app/lib/env/env.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod';
+import {z} from 'zod';
 
 const env = {
   DATABASE_URL: process.env.DATABASE_URL,
@@ -7,12 +7,12 @@ const env = {
 };
 
 const envSchema = z.object({
-  DATABASE_URL: z.optional(z.string().url()),
-  OPEN_AI_API_KEY: z.optional(z.string().min(1)),
-  NEXT_PUBLIC_GITHUB_API_URL: z.optional(z.string().url()),
+  DATABASE_URL: z.string().url(),
+  OPEN_AI_API_KEY: z.string().min(1),
+  NEXT_PUBLIC_GITHUB_API_URL: z.string().url(),
 });
 
 export const validatedEnv = envSchema.parse(env);
 
-export const { DATABASE_URL, OPEN_AI_API_KEY, NEXT_PUBLIC_GITHUB_API_URL } =
+export const {DATABASE_URL, OPEN_AI_API_KEY, NEXT_PUBLIC_GITHUB_API_URL} =
   validatedEnv;
